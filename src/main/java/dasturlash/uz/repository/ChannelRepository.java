b272476deb100fa60ac83bd44a76ecd1e5567a80
YusupovJavaDev <baxtiyorking17@gmail.com>
2025-07-12 22:24:28 +0500

    channel methods

--- src/main/java/dasturlash/uz/repository/ChannelRepository.java
+++ src/main/java/dasturlash/uz/repository/ChannelRepository.java
@@ -1,9 +1,17 @@
 package dasturlash.uz.repository;
 
 import dasturlash.uz.entity.ChannelEntity;
+import dasturlash.uz.enums.GeneralStatus;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChannelRepository extends JpaRepository<ChannelEntity, String> {
+    @Modifying
+    @Transactional
+    @Query("update ChannelEntity c set c.status = ? where c.id = ?1 and c.visible = true ")
+    void changeStatus(String id, GeneralStatus name);
 }
--- src/main/java/dasturlash/uz/service/impl/ChannelServiceImpl.java
+++ src/main/java/dasturlash/uz/service/impl/ChannelServiceImpl.java
@@ -4,9 +4,11 @@ import dasturlash.uz.base.impl.BaseServiceImpl;
 import dasturlash.uz.dto.ChannelDTO;
 import dasturlash.uz.entity.ChannelEntity;
 import dasturlash.uz.enums.GeneralStatus;
+import dasturlash.uz.exp.AppBadException;
 import dasturlash.uz.mapper.ChannelMapper;
 import dasturlash.uz.repository.ChannelRepository;
 import dasturlash.uz.service.ChannelService;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,8 +16,13 @@ public class ChannelServiceImpl
         extends BaseServiceImpl<ChannelRepository, ChannelMapper, ChannelDTO, ChannelEntity>
         implements ChannelService {
 
-    public ChannelServiceImpl(ChannelRepository repository, ChannelMapper mapper) {
+    private final ChannelRepository channelRepository;
+    private final ChannelMapper channelMapper;
+
+    public ChannelServiceImpl(ChannelRepository repository, ChannelMapper mapper, ChannelRepository channelRepository, ChannelMapper channelMapper) {
         super(repository, mapper);
+        this.channelRepository = channelRepository;
+        this.channelMapper = channelMapper;
     }
 
     @Override
@@ -28,9 +35,14 @@ public class ChannelServiceImpl
         return null;
     }
 
+    @SneakyThrows
     @Override
     public ChannelDTO changeStat(String id, GeneralStatus stat) {
-        return null;
+        if (!id.isBlank() && stat != null) {
+            channelRepository.changeStatus(id,stat);
+            return channelMapper.toDTO(channelRepository.findById(id).orElseThrow());
+        }
+        throw new AppBadException("WTH is that piece of shit!");
     }
 
     @Override
