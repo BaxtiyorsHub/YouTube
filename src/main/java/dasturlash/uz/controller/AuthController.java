76e58033a5ea41870fac6e61494edef9d9e34eac
YusupovJavaDev <baxtiyorking17@gmail.com>
2025-07-15 14:17:09 +0500

    loggin + flyway

--- src/main/java/dasturlash/uz/controller/AuthController.java
+++ src/main/java/dasturlash/uz/controller/AuthController.java
@@ -4,8 +4,11 @@ import dasturlash.uz.dto.AuthDTO;
 import dasturlash.uz.dto.AuthorizationDTO;
 import dasturlash.uz.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/auth")
@@ -13,22 +16,26 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private static final Logger log = (Logger) LoggerFactory.getLogger(AuthController.class);
 
     @PostMapping("/register")
     public ResponseEntity<String> register(
-            @RequestBody AuthDTO dto
-    ){
+            @RequestBody AuthDTO dto) {
+        log.fine("debug register");
+        log.info("info register");
+        log.warning("warn register");
+        log.severe("error register");
         return ResponseEntity.ok(authService.createRegistrationCode(dto));
     }
 
     @PutMapping("/verification/{token}")
-    public ResponseEntity<String> verification(@PathVariable String token){
+    public ResponseEntity<String> verification(@PathVariable String token) {
         return ResponseEntity.ok(authService.verification(token));
     }
 
     @GetMapping("/login")
     public ResponseEntity<AuthDTO> login(
-            @RequestBody AuthorizationDTO authorization){
+            @RequestBody AuthorizationDTO authorization) {
         return ResponseEntity.ok(authService.login(authorization));
     }
 }
